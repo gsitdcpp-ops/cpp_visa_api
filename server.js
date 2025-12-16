7590b33653f6b48b90504300fe4943293563e761
cpp_visa_api <gsitdcpp@gmail.com>
2025-12-16 15:05:46 +0700

    7

--- server.js
+++ server.js
@@ -3,18 +3,9 @@ const app = express();
 
 app.use(express.json());
 
-// Base URL : Homepage Of Website
 app.get("/", (req, res) => {
   res.json({
-    message: "✅ Server is running!",
-    timestamp: new Date().toISOString(),
-    success: true,
-  });
-});
-
-app.get("/myapi", (req, res) => {
-  res.json({
-    message: "✅ Server is running my api!",
+    message: "✅ Server MrBlack is connected! Working as well!",
     timestamp: new Date().toISOString(),
     success: true,
   });
@@ -23,5 +14,5 @@ app.get("/myapi", (req, res) => {
 const PORT = process.env.PORT || 8888;
 app.listen(PORT, () => {
   console.log(`Local: http://localhost:${PORT}`);
-  console.log("Production: https://oneev-kh-api.vercel.app/");
+  console.log("Production: https://cpp-visa-api.vercel.app/");
 });
